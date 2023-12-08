b4fd23978da3de3aaca43b6b9a83377ca4392309
amandin <123622161+amawdiin@users.noreply.github.com>
2023-12-08 17:01:24 +0600

    file organization sitll bad tho ong

--- src/python/unit_tests.py
+++ src/python/unit_tests.py
@@ -1,15 +1,5 @@
 from main import *
 
-# class test(nbt):
-#     def shift_array(syntax):
-#         words = nbt.split_into_words(syntax)
-#         source_type, source, words = nbt.get_source_type_and_source(words)
-#         path = words.pop(0) if words else ""
-#         return f'''
-# data modify {source_type} {source} {path} append from {source_type} {source} {path}[0]
-# data remove {source_type} {source} {path}[0]'''
-# nbt = test
-
 correct = [
     'data get block ~ ~ ~ Items',
     'data merge entity @s {NoAI:1b,Invulnerable:1b}',
