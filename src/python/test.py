438bce650847e7ae1b9f927762702cad7c2dfc9c
amandin <123622161+amawdiin@users.noreply.github.com>
2023-12-13 11:59:19 +0600

    make actual tokenizer for everything rather tokenizing for specific functions

--- src/python/test.py
+++ src/python/test.py
@@ -5,3 +5,12 @@ def test():
 
 print(test())
 
+# print(Nbt.get('0 0 0 Items[0]tag.'))
+test_case = "main some.string append string main storage.path 0 1"
+# test_case = "0 0 0 Items[0] set from 0 0 0 SelectedItem"
+
+print(f'{Nbt.tokenize(test_case)}')
+print("")
+print(f"{Nbt.modify(test_case)}')")
+
+
--- src/unit_tests.py
+++ src/unit_tests.py
@@ -8,8 +8,8 @@ correct = [
     'data modify storage main some.tag.array append value 1b',
     'data modify storage main array append from storage temp tempArray[0]',
     'data modify storage main some.path set from entity @s[type=player,tag=!this] SelectedItem.tag.pages',
-    'data modify storage main some.string set string block 0 0 0 Items[0]  0 1',
-    'data modify storage main some.string append string storage main storage.path  0 1',
+    'data modify storage main some.string set string block 0 0 0 Items[0] 0 1',
+    'data modify storage main some.string append string storage main storage.path 0 1',
     'data remove storage main some.array[0]',
     'data merge block 0 0 0 {Items:[{Slot:0b,id:"minecraft:stone",Count:1b}]}',
     'data modify storage main array[{"somefilter":1b}][0].tag.array[-1][0] set value 1b',
@@ -22,21 +22,21 @@ def test_all():
     global correct
     test_result = []
     test_result += [
-        nbt.get("~ ~ ~ Items"),
-        nbt.merge("@s {NoAI:1b, Invulnerable:1b}"),
-        nbt.merge("@s {NoAI:1b, Invulnerable:1b}"),
-        nbt.modify("main some.tag set 1b"),
-        nbt.modify("main some.tag.array append 1b"),
-        nbt.modify("main array append from temp tempArray[0]"),
-        nbt.modify("main some.path set from @s[type=player, tag=!this] SelectedItem.tag.pages"),
-        nbt.modify('main some.string set string 0 0 0 Items[0] 0 1'),
-        nbt.modify('main some.string append string main storage.path 0 1'),
-        nbt.remove('main some.array[0]'),
-        nbt.merge('0 0 0 {Items:[{Slot:0b,id:"minecraft:stone",Count:1b}]}'),
-        nbt.modify('main array[{"somefilter":1b}][0].tag.array[-1][0] set 1b'),
-        nbt.remove('0 0 0 Items[1]'),
-        nbt.shift_array('main path.to.array'),
-        nbt.get('f81d4fae-7dec-11d0-a765-00a0c91e6bf6 SelectedItem.Count')
+        Nbt.get("~ ~ ~ Items"),
+        Nbt.merge("@s {NoAI:1b, Invulnerable:1b}"),
+        Nbt.merge("@s {NoAI:1b, Invulnerable:1b}"),
+        Nbt.modify("main some.tag set 1b"),
+        Nbt.modify("main some.tag.array append 1b"),
+        Nbt.modify("main array append from temp tempArray[0]"),
+        Nbt.modify("main some.path set from @s[type=player, tag=!this] SelectedItem.tag.pages"),
+        Nbt.modify('main some.string set string 0 0 0 Items[0] 0 1'),
+        Nbt.modify('main some.string append string main storage.path 0 1'),
+        Nbt.remove('main some.array[0]'),
+        Nbt.merge('0 0 0 {Items:[{Slot:0b,id:"minecraft:stone",Count:1b}]}'),
+        Nbt.modify('main array[{"somefilter":1b}][0].tag.array[-1][0] set 1b'),
+        Nbt.remove('0 0 0 Items[1]'),
+        Nbt.shift_array('main path.to.array'),
+        Nbt.get('f81d4fae-7dec-11d0-a765-00a0c91e6bf6 SelectedItem.Count')
     ]
     for test, command in zip(test_result, correct):
         index = test_result.index(test)+1
@@ -46,5 +46,4 @@ def test_all():
             print(f'test {index} passed')
 
 test_all()
-
 
